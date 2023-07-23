736bb15be6e2c4ecf959e995492f2d06b139a6b7
Jayanth Kumar <jayanth826833@gmail.com>
2023-07-23 15:25:22 +0530

    Calc file added

---
+++ Calc.java
@@ -0,0 +1,11 @@
+class Calc {
+    public int add(int a,int b) {
+        return a+b;
+    }
+    static void show(){
+        System.out.println("From show method");
+    }
+    public Calc(){
+        System.out.println("from Constructor ");
+    }
+}
--- firstProgram.java
+++ firstProgram.java
@@ -1,14 +1,4 @@
-class Calc {
-    public int add(int a,int b) {
-        return a+b;
-    }
-    static void show(){
-        System.out.println("From show method");
-    }
-    public Calc(){
-        System.out.println("from Constructor ");
-    }
-}
+
 
 class A {
     public A() {
