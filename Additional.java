4ad9b5fa83d58553dcd0050a6a13fc58616f5e80
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-03-01 23:54:20 +0530

    Strip and Trim example added

--- Additional.java
+++ Additional.java
@@ -1,2 +1,8 @@
 Reapeat function
-  String j="Jay".repeat(10)
+  String j="Jay".repeat(10);
+
+
+  // Trim and Strip
+  // Strip is advanced version od trim,trim is used to trim only ascii charecters and strip will work for all (language) characters
+  String j="Jay   ".trim();
+  String k="Kumar ".strip();
