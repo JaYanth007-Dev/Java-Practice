14c9bc80e0d395e474bb6e516941ea3dc229494c
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-03-08 23:33:45 +0530

    FI static function Example Added

--- Java8/Java8.java
+++ Java8/Java8.java
@@ -9,6 +9,8 @@ public class Java8 {
     public static void main(String[] args) {
         Java8Interface obj = (name) -> System.out.println("Hello " + name);
         obj.greet("Jayanth");
+        int v = Java8Interface.add(9, 10);
+        System.out.println("Sum is " + v);
 
     }
 }
