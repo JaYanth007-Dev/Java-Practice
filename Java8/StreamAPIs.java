f0344efa7418915afa4867752b7190218ab18728
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-03-08 23:57:49 +0530

    Map,Filter,Method Reference Example added

--- Java8/StreamAPIs.java
+++ Java8/StreamAPIs.java
@@ -2,11 +2,17 @@ package Java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamAPIs {
     public static void main(String[] args) {
-        List<String> values = Arrays.asList("Apple", "Banana", "Cherry", "Date", "Elderberry");
-        StreamAPIInterface obj=(name)->System.out.println(name);
-        values.stream().forEach(obj::printing);
+        List<String> list1 = Arrays.asList("Apple", "Banana", "Cherry", "Date", "Elderberry");
+        StreamAPIInterface obj = (name) -> System.out.println(name);
+        list1.stream().forEach(obj::printing);
+
+        List<String> list2 = Arrays.asList("Apple", "ant", "Cherry", "Cat", "Dog", "Aeroplane");
+        List<String> values = list2.stream().map(String::toUpperCase).filter(item -> item.startsWith("A"))
+                .collect(Collectors.toList());
+        System.out.println(values);
     }
 }
