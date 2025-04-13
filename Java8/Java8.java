52c235b5f16fd4018880f1879cafcc096e78b648
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-04-13 23:59:31 +0530

    Update Java8.java

--- Java8/Java8.java
+++ Java8/Java8.java
@@ -7,6 +7,7 @@
  */
 public class Java8 {
     public static void main(String[] args) {
+        // Creating implementation of abstract method present in Java8Interface (functional Interface)
         Java8Interface obj = (name) -> System.out.println("Hello " + name);
         obj.greet("Jayanth");
         int v = Java8Interface.add(9, 10);
